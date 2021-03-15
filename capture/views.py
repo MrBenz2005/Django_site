7a277c774d7e495aa59e45728298ba4431134804
MrBenz <dkostya05@gmail.com>
2021-03-15 15:18:38 +0300

    .

--- capture/views.py
+++ capture/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
+def capture(request):
 	return HttpResponse("<h3>Если видишь значит работает</h3>")
 # Create your views here.
