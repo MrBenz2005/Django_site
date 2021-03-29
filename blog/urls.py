ecd1f6eb476c3ba5a45cee327471a48aa3eb004b
MrBenz <dkostya05@gmail.com>
2021-03-29 11:32:20 +0300

    /

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
 from . import views
-from django.contrib.auth.views import LogoutView
-from django.conf.urls import url
 
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('rating/', views.rating, name='rating'),
     path('accounts/', include('allauth.urls')),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 def main(request):
     return render(request, 'blog/mainPage.html', {})
 
+def rating(request):
+    return render(request, 'blog/dummy.html', {})
+
+
 
 # Create your views here.
