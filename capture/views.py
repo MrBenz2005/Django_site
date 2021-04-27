fe42a5a04d806268017fc467b81a4e3057cfd58e
MrBenz <dkostya05@gmail.com>
2021-04-27 20:59:11 +0300

    /

--- capture/views.py
+++ capture/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-def capture(request):
-	return render(request, 'capture/choose_capture.html', {})
+from captcha.image import ImageCaptcha
+import random
 
 def picture(request):
     return render(request, 'blog/mainPage.html', {})
-# Create your views here.
+
+def capture(request):
+	image = ImageCaptcha()
+
+	symbols = ['QWERTYUIOPLKJHGFDSAZXCVBNM1234567890']
+	vyvod = ' '
+	for i in range(6):
+		vyvod += symbols[0][random.randint(0, len(symbols[0]))]
+
+	data = image.generate(vyvod)
+	capture_write = image.write(vyvod, 'captcha1.png')
+	return 	render(request, 'capture/choose_capture.html', {'vyvod': vyvod})
