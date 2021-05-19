53bfcff8dca482396985a3e222ea86d1b71c1ce5
MrBenz <dkostya05@gmail.com>
2021-05-19 09:20:07 +0300

    /

--- capture/views.py
+++ capture/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from captcha.image import ImageCaptcha
+from .forms import UserForm
 import random
+import os
 
 def picture(request):
     return render(request, 'blog/mainPage.html', {})
@@ -12,10 +14,32 @@ def capture(request):
 	symbols = ['QWERTYUIOPLKJHGFDSAZXCVBNM1234567890']
 	vyvod = ' '
 	for i in range(6):
-		vyvod += symbols[0][random.randint(0, len(symbols[0]))]
+		vyvod += symbols[0][random.randint(0, len(symbols[0]) - 1)]
+	print(f'CWD: {os.getcwd()}')
 
 	data = image.generate(vyvod)
-	capture_write = image.write(vyvod, 'captcha1.png')
+	image_path = "captcha1.png"
+	image.write(vyvod, f"capture/static/{image_path}")
 
-	return 	render(request, 'capture/choose_capture.html', {'img_captcha': data})
 
+	return 	render(request, 'capture/choose_capture.html', {'image_path': image_path})
+
+
+def index(request):
+  submitbutton= request.POST.get("submit")
+
+  firstname=''
+  lastname=''
+  emailvalue=''
+
+  form= UserForm(request.POST or None)
+  if form.is_valid():
+        firstname= form.cleaned_data.get("first_name")
+        lastname= form.cleaned_data.get("last_name")
+        emailvalue= form.cleaned_data.get("email")
+
+  context= {'form': form, 'firstname': firstname, 
+            'lastname':lastname, 'submitbutton': submitbutton,
+            'emailvalue':emailvalue}
+
+  return render(request, 'capture/templates/capture/choose_capture.html', context)
